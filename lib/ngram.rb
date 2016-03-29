4bbe00723a10594133323b33e4cc90d958ad74c9
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-29 23:46:17 +0530

    Initial commit
    Definition of n-gram.

--- lib/ngram.rb
+++ lib/ngram.rb
@@ -8,3 +8,9 @@
 # by Prabhat Kumar, http://prabhatkumar.org/
 # Date          : 29-March-2016
 # ==========================================
+# In the fields of computational linguistics and probability,
+# an n-gram is a contiguous sequence of n items from a given
+# sequence of text or speech. The items can be phonemes,
+# syllables, letters, words or base pairs according to the application.
+# The n-grams typically are collected from a text or speech corpus.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
