821b81d110ac1cd03538d324095dffd124feb592
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-29 23:50:22 +0530

    Initial commit
    Copyright © 2016, Prabhat Kumar. All rights reserved.

--- lib/ngram.rb
+++ lib/ngram.rb
@@ -14,3 +14,22 @@
 # syllables, letters, words or base pairs according to the application.
 # The n-grams typically are collected from a text or speech corpus.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ================================= n-gram® ==============================
+# ------------------------------------------------------------------------
+# Copyright © 2016, Prabhat Kumar. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0, (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       	  http://www.apache.org/licenses/LICENSE-2.0
+#                           	  or
+#     https://github.com/iamprabhat/n-gram/blob/master/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ========================================================================
