5e22bbf5a6ffa7201385597c449979e331d192d7
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-29 20:14:05 +0530

    Initial commit
    n-gram®: [n-gram NLP toolkit in Ruby], by Prabhat Kumar, http://prabhatkumar.org/.

--- lib/ngram.rb
+++ lib/ngram.rb
@@ -1,1 +1,10 @@
 #!/usr/bin/env ruby
+
+
+# n-gram®
+# ------------------------------------------
+# n-gram NLP toolkit in Ruby.
+# ------------------------------------------
+# by Prabhat Kumar, http://prabhatkumar.org/
+# Date          : 29-March-2016
+# ==========================================
